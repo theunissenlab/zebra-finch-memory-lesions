ee77fb8d82940436150f654db0f7f9a786c086c9
kevin <thekevinyu@gmail.com>
2020-10-21 13:39:39 -0700

    Fix load_data functions and correct subject name in LesionData.csv

--- code/load_data.py
+++ code/load_data.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 import numpy as np
@@ -6,7 +7,7 @@ import wavio
 
 
 CODEDIR, _ = os.path.split(__file__)
-DATADIR = os.path.join(CODEDIR, "..")
+DATADIR = os.path.join(CODEDIR, "..", "data")
 
 
 def load_behavioral_data():
@@ -25,35 +26,49 @@ def load_lesion_data():
     """Load pandas DataFrame from TrialData.csv"""
     return pd.read_csv(
         os.path.join(DATADIR, "behavior", "LesionData.csv"),
+        converters={
+            "Date_Lesioned": lambda d: pd.to_datetime(d).date()
+        }
     )
 
 
 def load_spike_data():
-    df = pd.read_pickle(os.path.join("data", "ephys", "UnitData.pkl"))
+    df = pd.read_pickle(os.path.join(DATADIR, "ephys", "UnitData.pkl"))
+
+    # TODO eventually apply these to the original dataframe so we dont have to compute it every time we load
     df["n_trials"] = df.apply(lambda row: np.sum(row["good_trials"]), axis=1)
     df["spike_times"] = df.apply(lambda row: np.array(row["spike_times"])[np.array(row["good_trials"])], axis=1)
+    df["subject"] = df["site"].apply(lambda site: site.split("_")[0])
+    def _parse_date(site):
+        date_str = site.split("_")[1]
+        year = 2000 + int(date_str[:2])
+        month = int(date_str[2:4])
+        day = int(date_str[4:])
+        return datetime.date(year, month, day)
+
+    df["date"] = df["site"].apply(_parse_date)
     df = df.drop("good_trials", axis=1)
     df = df.set_index(["unit_id", "file"]).sort_index()
     return df
 
 
 def load_stim_data():
-    df = pd.read_pickle(os.path.join("data", "ephys", "StimData.pkl")
+    df = pd.read_pickle(os.path.join(DATADIR, "ephys", "StimData.pkl"))
     return df
 
 
 def load_behavior_stimulus(stim_file):
     """Return wavio.Wav object for given stimulus filename"""
-    return wavio.read(os.path.join("data", "behavior", "stimuli", stim_file))
+    return wavio.read(os.path.join(DATADIR, "behavior", "stimuli", stim_file))
 
 
 def load_ephys_stimulus(stim_file):
     """Return wavio.Wav object for given stimulus filename"""
-    return wavio.read(os.path.join("data", "ephys", "stimuli", stim_file))
+    return wavio.read(os.path.join(DATADIR, "ephys", "stimuli", stim_file))
 
 
 def load_unit_waveforms(unit_id):
-    return np.load(os.path.join("data", "ephys", "unit_waveforms", unit_id))
+    return np.load(os.path.join(DATADIR, "ephys", "unit_waveforms", unit_id))
 
 
 __all__ = [
